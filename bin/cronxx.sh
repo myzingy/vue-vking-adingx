af03ad095e1fb414e167f932748109825cb53007
root <root@localhost.localdomain>
2017-06-12 05:37:13 -0400

    cronxx

--- bin/cronxx.sh
+++ bin/cronxx.sh
@@ -2,18 +2,17 @@
 delayed_ajax() {
   local url=$1
   local callback=$2
-  local seconds=$3
-  sleep $seconds
   /usr/bin/curl -s "$url" | "$callback"
 }
 
 my_handler() {
   # Read from stdin and do something.
   # E.g. just append to a file:
-  # > /dev/null 2>&1 &
-  cat >> /tmp/some_file.txt
+  cat > /dev/null 2>&1
+  #cat >> /tmp/some_file.txt
 }
 
-for delay in {1..12}; do
-  delayed_ajax http://127.0.0.1/facebook/wwwroot/apido/cron.demon my_handler 5 &
+for delay in {1..11}; do
+  delayed_ajax http://127.0.0.1/facebook/wwwroot/apido/cron.demon my_handler &
+  sleep 5
 done
