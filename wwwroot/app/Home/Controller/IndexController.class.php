a4cdea171c8c7920ab31e5a868132fbd66d6d18f
wangfan <251941666@qq.com>
2017-06-13 13:45:23 +0800

    3点执行获取历史数据

--- wwwroot/app/Home/Controller/IndexController.class.php
+++ wwwroot/app/Home/Controller/IndexController.class.php
@@ -6,13 +6,6 @@ class IndexController extends Controller {
 		parent::__construct();
 	}
 	function index(){
-	    dump([NOW_TIME,date("Y-m-d H:i:s",NOW_TIME)]);
-	    $time=getDayTime("03:00:00",0);
-        dump([$time,date("Y-m-d H:i:s",$time)]);
-        $time=getDayTime("04:00:00",-1);
-        dump([$time,date("Y-m-d H:i:s",$time)]);
-        $time=getDayTime("05:00:00",1);
-        dump([$time,date("Y-m-d H:i:s",$time)]);
-        //die('<meta http-equiv="refresh" content="0;url='.url('../wwwrootdist/').'"> ');
+	    die('<meta http-equiv="refresh" content="0;url='.url('../wwwrootdist/').'"> ');
     }
 }
