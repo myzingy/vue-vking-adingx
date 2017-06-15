9225d08add4f0fbe1f993e7effa6d8d3b9278683
wangfan <251941666@qq.com>
2017-06-15 08:52:01 +0800

    + 'fb_account'=>'Jeulia Jewelry' 所有ad

--- wwwroot/app/Common/Conf/facebookads.php
+++ wwwroot/app/Common/Conf/facebookads.php
@@ -6,8 +6,11 @@ return array(
             'app_secret'=>'2b8cf7d36a3a389505d7a745449adc86',
             'access_tokens'=>'EAABeuMl0aOwBABsrRnMU82d8BKjERXJ8Y4cqajP1elZB24FETbrjN6T75LsY0gKNZA3nhO4k3yZC5UFCcXqIGsQJH3c5yRwBgu2cV1hr8hasZBnJbXfcDlGJ2MmXIHc0xB678Tott7i8V2lKTTBrhWK3k0Dx2Vo7SA5kCz7cfVY0apWR9tob',
             'accounts'=>array(
-                array('account_id'=>'769185746596586','account_name'=>'BF-YM-Zhule-0310-01'),
-                array('account_id'=>'1593565507558990','account_name'=>'Jeulia'),
+                array('fb_account'=>'Jeulia Jewelry','account_id'=>'1593565507558990','account_name'=>'Jeulia'),
+                array('fb_account'=>'Jeulia Jewelry','account_id'=>'769185746596586','account_name'=>'BF-YM-Zhule-0310-01'),
+                array('fb_account'=>'Jeulia Jewelry','account_id'=>'564914007023762','account_name'=>'BF-YM-Xi\'an Zhule-0704-02'),
+                array('fb_account'=>'Jeulia Jewelry','account_id'=>'639275016254327','account_name'=>'BF-YM-Zhule-0817-01'),
+                array('fb_account'=>'Jeulia Jewelry','account_id'=>'909992302470836','account_name'=>'Xi\'an Zhule -1212-3'),
              ),
         ),
     )
