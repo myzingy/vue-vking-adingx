5e67f5329c61e6e41d861144f227547c10036e6b
wangfan <251941666@qq.com>
2017-06-21 15:02:15 +0800

    debug

--- wwwroot/app/Modules/accounts/insights/lib.class.php
+++ wwwroot/app/Modules/accounts/insights/lib.class.php
@@ -216,6 +216,7 @@ END;
         return $breakdowns_data?$breakdowns_data:$campaigns_data;
     }
     function postERP($url,$data){
+	    debug($url,$data);
 	    asyn_implement($url,$data,'POST');
     }
 }
