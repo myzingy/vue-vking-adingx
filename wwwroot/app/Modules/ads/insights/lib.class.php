19b637ab2799af672124de7d5ac63a18e30d32e0
wangfan <251941666@qq.com>
2017-07-14 17:29:27 +0800

    调整执行时间

--- wwwroot/app/Modules/ads/insights/lib.class.php
+++ wwwroot/app/Modules/ads/insights/lib.class.php
@@ -213,7 +213,7 @@ END;
             asyn('apido/asyn.flushAdsInsights',array('ad_id' => $ad_id,'ad_timespace'=>'last_14day','ac_id'=>$ac_id),null,
                 getDayTime("00:01:00"),-2);
             asyn('apido/asyn.flushAdsInsights',array('ad_id' => $ad_id,'ad_timespace'=>'lifetime','ac_id'=>$ac_id),null,
-                getDayTime("20:01:00"),0);
+                getDayTime("20:01:00",0),0);
 
         }
         if($ad_timespace=='lifetime'){
